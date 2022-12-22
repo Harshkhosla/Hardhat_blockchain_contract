ba9721e38c2d42717fbf364ca60cfc7c8cca8d03
harsh <harshkhosla9945@gmail.com>
2022-12-22 11:43:48 +0530

    second commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config()
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan")
+
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const RINKBEY_RPC_URL =process.env.RINKBEY_RPC_URL
 const PRIVATE_KEY =process.env.PRIVATE_KEY
+const ETHER_SCAN_API_KEY =process.env.ETHER_SCAN_API_KEY
 module.exports = {
+  defaultNetwork:"hardhat",
   networks:{
     rinkeby:{
       url:RINKBEY_RPC_URL,
@@ -13,4 +17,7 @@ module.exports = {
     },
   },
   solidity: "0.8.17",
+  etherscan:{
+    apikey:ETHER_SCAN_API_KEY,
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const{ethers}=require("hardhat")
+const{ethers ,run ,network }=require("hardhat")
 
 
 async function main(){
@@ -9,7 +9,41 @@ async function main(){
   const simpleStorage =await SimpleStorageFactory.deploy()
   await simpleStorage.deployed();
   console.log(`deployed contract to ${simpleStorage.address}`);
+  // console.log(network.config);
+  if(network.config.chainId===4 && process.env.ETHER_SCAN_API_KEY ){
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address,[])
+  }
+  const currentValue =await simpleStorage.retrieve()
+  console.log(currentValue);
+  const transctionresponse =await simpleStorage.store(7)
+  await transctionresponse.wait(1);
+  // update the current value
+  const updatevalue= await  simpleStorage.retrieve()
+  console.log(`updated value is${updatevalue}`); 
 }
+
+
+
+
+async function  verify  (contractAddress,arg){
+  console.log("verefying contract ....");
+  try{
+
+    await run("verify:verify",{
+      address:contractAddress,
+      constructorArguments:args,
+    })
+  }catch(e){
+    if(e.message.toLowerCase().includes("already verify")){
+      console.log("already verify");
+    }else{
+      console.log(e);
+    }
+  }
+  }
+
+
 
 main()
 .then(()=>process.exit(0))
