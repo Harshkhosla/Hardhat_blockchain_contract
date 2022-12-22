7f83b42b81e40bdc42833035e8ee49e534d7bb7e
harsh <harshkhosla9945@gmail.com>
2022-12-22 13:05:36 +0530

    completion

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,15 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./task/block-number")
 require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const RINKBEY_RPC_URL =process.env.RINKBEY_RPC_URL
 const PRIVATE_KEY =process.env.PRIVATE_KEY
 const ETHER_SCAN_API_KEY =process.env.ETHER_SCAN_API_KEY
+const COINMARKETCAP_API_KEY =process.env.COINMARKETCAP_API_KEY
 module.exports = {
   defaultNetwork:"hardhat",
   networks:{
@@ -24,5 +27,12 @@ module.exports = {
   solidity: "0.8.17",
   etherscan:{
     apikey:ETHER_SCAN_API_KEY,
-  }
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
 };
